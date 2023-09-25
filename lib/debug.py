69098cda51497ab46b69d5ad9640f54d7e6f8c3f
Kelvin Mue <kelvinmue@gmail.com>
2023-09-25 16:48:11 +0300

    Completed assignment

--- lib/debug.py
+++ lib/debug.py
@@ -1,7 +1,7 @@
-#!/usr/bin/env python3
-
 from config import CONN, CURSOR
 from song import Song
+
+
 
 
 if __name__ == '__main__':
--- lib/song.py
+++ lib/song.py
@@ -1,4 +1,42 @@
-from config import CONN, CURSOR
+# song.py
+
+import sqlite3
 
 class Song:
-    pass
+    def __init__(self, name, album, id=None):
+        self.id = id  # Assign id as None by default
+        self.name = name
+        self.album = album
+
+    def save(self):
+        conn = sqlite3.connect('music.db')
+        cursor = conn.cursor()
+        if self.id is None:
+            # Insert a new song with an auto-generated id
+            cursor.execute('INSERT INTO songs (name, album) VALUES (?, ?)', (self.name, self.album))
+            self.id = cursor.lastrowid  # Set the 'id' attribute to the last inserted row's ID
+        else:
+            # Update an existing song with a specific id
+            cursor.execute('UPDATE songs SET name=?, album=? WHERE id=?', (self.name, self.album, self.id))
+        conn.commit()
+        conn.close()
+
+    @classmethod
+    def create_table(cls):
+        conn = sqlite3.connect('music.db')
+        cursor = conn.cursor()
+        cursor.execute('''
+            CREATE TABLE IF NOT EXISTS songs (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT,
+                album TEXT
+            )
+        ''')
+        conn.commit()
+        conn.close()
+
+    @classmethod
+    def create(cls, name, album):
+        song = cls(name, album)
+        song.save()
+        return song
